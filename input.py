38f8fe1b3f06a404ade139ca8685bc1965999273
thorvalddox <thorvalddx94@gmail.com>
2016-08-31 14:17:29 +0200

    fixed attributeError

--- input.py
+++ input.py
@@ -30,14 +30,17 @@ class Joystick:
 
     def process(self):
         for event in self.device.read_loop():
-            if event.type_ == 0 and 288 <= event.code < 300:
-                yield "b{}".format(event.code),event.value
-            if event.type_ == 3:
-                axisindex = [0,1,2,5,16,17].index(event.code)
-                if event.code >= 16:
-                    self.axisvalues[axisindex] = event.value
-                else:
-                    self.axisvalues[axisindex] = -(event.value<63) + (event.value<192)
+            try:
+                if event.type_ == 0 and 288 <= event.code < 300:
+                    yield "b{}".format(event.code),event.value
+                if event.type_ == 3:
+                    axisindex = [0,1,2,5,16,17].index(event.code)
+                    if event.code >= 16:
+                        self.axisvalues[axisindex] = event.value
+                    else:
+                        self.axisvalues[axisindex] = -(event.value<63) + (event.value<192)
+            except AttributeError:
+                print(event)
 
 
     def get_axis_pole(self,index):
