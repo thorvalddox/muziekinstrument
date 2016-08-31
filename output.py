021211726f9dab34e501ad6aa7a426b7f6d371f0
thorvalddox <thorvalddx94@gmail.com>
2016-08-31 16:01:02 +0200

    removed stating and ending data to see if lost frames are improved

--- output.py
+++ output.py
@@ -48,7 +48,8 @@ class Soundhandler():
         ticklen = self.fs * (self.invoketime - self.prevtime)
         self.index += ticklen
         fadeframes = self.fs*fadetime
-        print(ticklen)
+        print("lost:",ticklen - ticks,"frames")
+        """
         try:
             prevvol = 0.9/len(self.freqprev)
         except ZeroDivisionError:
@@ -64,8 +65,8 @@ class Soundhandler():
             fade = prev and not now
             rase = now and not prev
             start = get_start(prevvol *(not rase),nowvol*(not fade),ticks,fadeframes)
-
-            yield (np.sin(2 * np.pi * (np.arange(ticks) + self.index) * freq / self.fs) * start * min(1,(220/freq)**2)).astype(np.float32)
+        """
+        yield (np.sin(2 * np.pi * (np.arange(ticks) + self.index) * freq / self.fs) * start * min(1,(220/freq)**2)).astype(np.float32)
         self.freqprev = self.freqlist.copy()
     def callback(self, in_data, frame_count, time_info, status):
         fulldata = list(self.get_next_data(frame_count,time_info["output_buffer_dac_time"]))
