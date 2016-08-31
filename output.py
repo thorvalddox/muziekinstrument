46101f2c570e8dd7e5bed9ec7d3039c11dc88c5f
thorvalddox <thorvalddx94@gmail.com>
2016-08-31 13:35:00 +0200

    Splitted calculating and displaying sound

--- output.py
+++ output.py
@@ -22,17 +22,20 @@ class Soundhandler():
                                   rate=fs,
                                   output=True,
                                   stream_callback=self.callback)
-        self.stream.frames_per_buffer = 1000
+        self.stream.frames_per_buffer = 1024
         self.w = open("test.txt","w")
 
         self.freqlist = set() #contains tuples: id,freq
         self.freqprev = set()
         self.index = 0
         self.next_wave = []
+        self.soundupdateThread = threading.Thread(None,self.update_next_wave)
+        self.soundupdateThread.start()
         print("Output ready")
 
     def update_next_wave(self):
-        self.next_wave = get_next_data()
+        while True:
+            self.next_wave = get_next_data(1024)
     def get_next_data(self, ticks, fadeframes=fs/20):
         self.index += ticks
         try:
@@ -55,7 +58,7 @@ class Soundhandler():
         self.freqprev = self.freqlist.copy()
     def callback(self, in_data, frame_count, time_info, status):
         print(frame_count)
-        fulldata = list(self.get_next_data(frame_count))
+        fulldata = list(self.next_wave)
         if fulldata:
             data = sum(fulldata)
         else:
